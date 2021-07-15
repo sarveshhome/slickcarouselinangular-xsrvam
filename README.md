27a4f850d6fd1f8924f3ecc24e046fc103e4b89e
sarveshhome <sarvesh.carlton@gmail.com>
2021-07-15 08:18:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# slickcarouselinangular-xsrvam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/slickcarouselinangular-xsrvam)
---
+++ src/app/imagesliderslick/imagesliderslick.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagesliderslick',
+  templateUrl: './imagesliderslick.component.html',
+  styleUrls: ['./imagesliderslick.component.css']
+})
+export class ImagesliderslickComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
